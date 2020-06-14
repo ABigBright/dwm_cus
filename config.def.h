b8b4932bcf64c9b22e9843229435896214be8c7f
briq <990647625@qq.com>
2020-06-14 22:29:33 +0800

    optimize : optimize the config.def.h

--- config.def.h
+++ config.def.h
@@ -3,28 +3,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_black[]       = "#000000";
-static const char col_yellow[]      = "#EEB422";
-static const char col_green[]       = "#00ff00";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int gappih      = 6;       /* horiz inner gap between windows */
+static const unsigned int gappiv      = 6;       /* vert inner gap between windows */
+static const unsigned int gappoh      = 6;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 6;       /* vert outer gap between windows and screen edge */
+static const int smartgaps            = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const char *fonts[]            = {"monospace:size = 10"};
+static const char dmenufont[]         = "monospace:size = 10";
+static const char col_gray1[]         = "#222222";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#eeeeee";
+static const char col_cyan[]          = "#005577";
+static const char col_black[]         = "#000000";
+static const char col_yellow[]        = "#EEB422";
+static const char col_green[]         = "#00ff00";
+static const unsigned int baralpha    = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
+static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_green },
@@ -74,13 +74,13 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *albertcmd[]  = { "albert", "show", NULL };
-static const char* audiomute[] = {"/home/briq/.dwm/scripts/audiomute.sh", NULL};
-static const char* audioraise[] = {"/home/briq/.dwm/scripts/audioraise.sh", "10"};
-static const char* audiolower[] = {"/home/briq/.dwm/scripts/audiolower.sh", "10"};
+static char dmenumon[2]            = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]       = { "alacritty", NULL };
+static const char *albertcmd[]     = { "albert", "show", NULL };
+static const char* audiomute[]     = {"/home/briq/.dwm/scripts/audiomute.sh", NULL};
+static const char* audioraise[]    = {"/home/briq/.dwm/scripts/audioraise.sh", "10"};
+static const char* audiolower[]    = {"/home/briq/.dwm/scripts/audiolower.sh", "10"};
 static const char* brightnessinc[] = {"/home/briq/.dwm/scripts/brightnessinc.sh", "10"};
 static const char* brightnessdec[] = {"/home/briq/.dwm/scripts/brightnessdec.sh", "10"};
 
