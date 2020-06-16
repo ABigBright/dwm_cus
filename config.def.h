3c254fe8af1bd0f5c3710536d2e3968f9e526112
briq <990647625@qq.com>
2020-06-16 16:30:08 +0800

    feat : add the viewontag plugin

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const unsigned int gappov      = 6;       /* vert outer gap between windo
 static const int smartgaps            = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
+static const Bool viewontag           = True;     /* Switch view on tag switch */
 static const char *fonts[]            = {"FiraCode Nerd Font:size = 16"};
 static const char dmenufont[]         = "FiraCode Nerd Font:size = 16";
 static const char col_gray1[]         = "#222222";
--- dwm.c
+++ dwm.c
@@ -2008,6 +2008,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag)
+			view(arg);
 	}
 }
 
