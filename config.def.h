20eeb77c5a035b0668376412ecb842015b47992d
briq <990647625@qq.com>
2020-06-21 21:54:04 +0800

    feat : adjust the dwm font

--- config.def.h
+++ config.def.h
@@ -14,8 +14,8 @@ static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const Bool viewontag           = True;     /* Switch view on tag switch */
 static const int decorhints           = 1;        /* 1 means respect decoration hints */
-static const char *fonts[]            = {"FiraCode Nerd Font:size = 16"};
-static const char dmenufont[]         = "FiraCode Nerd Font:size = 16";
+static const char *fonts[]            = {"FiraCode Nerd Font:size = 12"};
+static const char dmenufont[]         = "FiraCode Nerd Font:size = 12";
 static const char col_gray1[]         = "#222222";
 static const char col_gray2[]         = "#444444";
 static const char col_gray3[]         = "#bbbbbb";
@@ -49,8 +49,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp"            , NULL, NULL, 0     , 1, -1 },
+	{ "Firefox"         , NULL, NULL, 1 << 8, 0, -1 },
 };
 
 /* layout(s) */
