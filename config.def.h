ec8e3943772044b678e33fd2b0564b21f873f3f7
briq <990647625@qq.com>
2020-09-10 09:16:31 +0800

    other : modify the win border width and window gaps
    Signed-off-by: briq <990647625@qq.com>

--- config.def.h
+++ config.def.h
@@ -3,12 +3,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int borderpx    = 1;        /* border pixel of windows */
 static const unsigned int snap        = 32;       /* snap pixel */
-static const unsigned int gappih      = 6;       /* horiz inner gap between windows */
-static const unsigned int gappiv      = 6;       /* vert inner gap between windows */
-static const unsigned int gappoh      = 6;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov      = 6;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih      = 3;       /* horiz inner gap between windows */
+static const unsigned int gappiv      = 3;       /* vert inner gap between windows */
+static const unsigned int gappoh      = 3;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 3;       /* vert outer gap between windows and screen edge */
 static const int smartgaps            = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 0;        /* 0 means bottom bar */
