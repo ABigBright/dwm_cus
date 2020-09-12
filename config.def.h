e16f863c33a171971ab1249576b848ee488df036
briq <990647625@qq.com>
2020-09-12 16:17:51 +0800

    Revert "feat : use rofi to substitude albert"
    This reverts commit f75aa3ccb888028130eb4bdf825a5d3dff9a1a73.

--- config.def.h
+++ config.def.h
@@ -100,7 +100,6 @@ static char dmenumon[2]               = "0"; /* component of dmenucmd, manipulat
 static const char *dmenucmd[]         = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]          = { "alacritty", NULL };
 static const char *albertcmd[]        = { "albert", "show", NULL };
-static const char *roficmd[]          = { "rofi", "-show", "run",  NULL };
 static const char *rangercmd[]        = { "alacritty", "-e", "ranger", NULL };
 static const char *trayercmd[]        = { "/home/briq/.dwm/scripts/trayertoggle.sh", NULL };
 static const char *i3lockcmd[]        = { "i3lock", "-i", "~/.dwm/scripts/lock.png", NULL };
@@ -115,7 +114,7 @@ static const char* screenshottoggle[] = {"flameshot", "gui",  NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = albertcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
     { MODKEY,                       XK_f,      spawn,          {.v = rangercmd } },
 	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = trayercmd } },
