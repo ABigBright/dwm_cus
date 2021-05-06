d5623cbca235053910b7ff57aca6947986bfae8f
briq <990647625@qq.com>
2021-05-06 22:44:41 +0800

    feat : open chrome browser in specific tag

--- config.def.h
+++ config.def.h
@@ -67,9 +67,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp"     , NULL, NULL, 0     , 1, -1 },
-	{ "Firefox"  , NULL, NULL, 1 << 1, 0, -1 },
-	{ "Chromium" , NULL, NULL, 1 << 1, 0, -1 }, // start chrome in tag 1
+	{ "Gimp"         , NULL, NULL, 0     , 1, -1 },
+	{ "Firefox"      , NULL, NULL, 1 << 1, 0, -1 },
+	{ "Chromium"     , NULL, NULL, 1 << 1, 0, -1 }, // start chrome in tag 1
+	{ "Google-chrome", NULL, NULL, 1 << 1, 0, -1 }, // start chrome in tag 1
 };
 
 /* layout(s) */
