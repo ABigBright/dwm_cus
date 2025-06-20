98287ca98a7d73b7904b3d7c6e892c7b6c548673
briq <990647625@qq.com>
2025-06-20 11:12:34 +0800

    feat: use fontconfig font, no border pixel
    Signed-off-by: briq <990647625@qq.com>

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int borderpx    = 0;        /* border pixel of windows */
 static const unsigned int snap        = 32;       /* snap pixel */
 static const unsigned int gappih      = 4;       /* horiz inner gap between windows */
 static const unsigned int gappiv      = 4;       /* vert inner gap between windows */
@@ -14,8 +14,8 @@ static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 0;        /* 0 means bottom bar */
 static const Bool viewontag           = True;     /* Switch view on tag switch */
 static const int decorhints           = 0;        /* 1 means respect decoration hints */
-static const char *fonts[]            = {"FiraCode Nerd Font Mono:size=10"};
-static const char dmenufont[]         = "FiraCode Nerd Font Mono:size=10";
+static const char *fonts[]            = {"monospace:size=10"};
+static const char dmenufont[]         = "serif:size=10";
 static const char col_gray1[]         = "#222222";
 static const char col_gray2[]         = "#444444";
 static const char col_gray3[]         = "#bbbbbb";
